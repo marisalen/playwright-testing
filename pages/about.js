092cfedce88eed1fb34d1e69ae38eefe5be06e20
marisalen <mlenarduzzi1@my.bcit.ca>
2023-01-24 10:56:08 -0800

    initial

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function about() {
+    return(
+       <>About Page
+            <Link href='/'>
+            Go Back</Link>
+        </>
+
+    )}
---
+++ pages/grades.js
@@ -0,0 +1,4 @@
+export default function grades() {
+    return(
+        <>Grades Page</>
+    )}
